d3c0ce84986e881b04fc117672cdb783293ce532
highhaoru <jin.ha@northeastern.edu>
2022-11-30 16:52:15 -0500

    final

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ import cors from 'cors'
 import mongoose from "mongoose";
 
 // mongoose.connect('mongodb://localhost:27017/tuiter');
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-                          || 'mongodb://localhost:27017/tuiter';
+// const CONNECTION_STRING = 'mongodb+srv://Haohighru:57310808SZdl!@cluster0.cm75ilh.mongodb.net/?retryWrites=true&w=majority'//process.env.DB_CONNECTION_STRING
+//                          || 'mongodb://localhost:27017/tuiter';
+const CONNECTION_STRING =    'mongodb+srv://alice:alice123@cluster0.cm75ilh.mongodb.net/?retryWrites=true&w=majority'
+console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
